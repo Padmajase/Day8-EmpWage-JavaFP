7ec4f9d6a4b6687f9cec5287af7d15dbd5c9d201
Padmajase <padmajapawar7@gmail.com>
2022-04-16 22:57:06 -0700

    Added : Employee Wage UC2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,24 +1,27 @@
 import java.util.Random;
 
-class EmployeeWage {
+class UC2 {
+
+	static int wagePerHr = 20;
+	static int fullTimeHr = 8;
 	public static void main(String args[]) {
 
 	System.out.println("Welcome !");
 
 	Random rand = new Random();
-	int isEmployee = rand.nextInt(2); 
+	int isEmployee = rand.nextInt(2);	
 	System.out.println(isEmployee);
 
-	/**  Checking Employee is Present or Absent **/
-	
-	if(isEmployee == 1)
-	{
+	/** Calculating Daily  Employee Wage **/
+
+	if(isEmployee == 1){
 		System.out.println("Employee is Present");
-	}
-	else
-	{
+		System.out.println("Daily Employee Wage = " +(wagePerHr * fullTimeHr));
+		
+	}else {
 		System.out.println("Employee is Absent");
+		System.out.println("Daily Employee Wage = 0 " );
 	}
 
-	}
+          }
 }
